c27e30e3af14638dec306d5ffe01a3a735e89080
Raumain <romaintibo6@gmail.com>
2025-10-26 12:58:41 +0100

    fix: project selection padding

--- src/components/projects-selection.tsx
+++ src/components/projects-selection.tsx
@@ -5,11 +5,16 @@ import SectionHeader from "./section-header";
 export default function ProjectsSelection() {
 	return (
 		<section
-			className="w-full bg-[#191919] py-6 sm:py-8 px-4 sm:px-6 lg:px-8 -mt-8"
+			className="w-full bg-[#191919] py-6 sm:py-8 px-4 sm:px-8 md:px-16 lg:px-24 xl:pl-32 -mt-8"
 			aria-labelledby="projects-heading"
 		>
-			<div className="px-4 sm:px-8 md:px-16 lg:px-24 mx-auto">
-				<SectionHeader headingId="projects-heading" highlightedText="#projets" tag="" title="Sélection de"/>
+			<div className="mx-auto">
+				<SectionHeader
+					headingId="projects-heading"
+					highlightedText="#projets"
+					tag=""
+					title="Sélection de"
+				/>
 				<div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 md:gap-10 lg:gap-12 mx-auto">
 					{data.slice(0, 4).map((project) => (
 						<ProjectCard key={project.id} project={project} />
